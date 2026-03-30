ae99d9f0338c87120f667946f37e28df645f5eb3
t-cobrien <55260388+catherineobrien@users.noreply.github.com>
2026-03-30 11:28:39 +0200

    Added device_arg

--- test/test_mmposewholebody.sh
+++ test/test_mmposewholebody.sh
@@ -12,9 +12,11 @@ bash "$SCRIPT_DIR/download_test_data.sh"
 
 # Parse command line arguments
 USE_SLURM=""
+DEVICE_ARG=""
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --slurm) USE_SLURM="--slurm"; shift ;;
+        --device) DEVICE_ARG="--device $2"; shift 2 ;;
         *) echo "Unknown argument: $1" >&2; exit 1 ;;
     esac
 done
@@ -27,7 +29,8 @@ mkdir -p "$OUTPUT_DIR"
 bash "$REPO_DIR/videos_to_poses.sh" \
     --type mmposewholebody \
     --input "$INPUT_DIR" \
-    --output "$OUTPUT_DIR"
+    --output "$OUTPUT_DIR" \
+    $DEVICE_ARG
 
 MMPOSEWHOLEBODY_VENV="$REPO_DIR/estimators/tools/mmposewholebody/venv"
 
